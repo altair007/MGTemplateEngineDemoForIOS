9577c9170b3aa3d14915de17ede9bd8c32ae32b7
颜风 <1178328901@qq.com>
2014-07-12 09:38:36 +0800

    Initial Commit

---
+++ MGTemplateEngineDemoForIOS/YFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YFAppDelegate.h
+//  MGTemplateEngineDemoForIOS
+//
+//  Created by 颜风 on 14-7-12.
+//  Copyright (c) 2014年 Shadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MGTemplateEngineDemoForIOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MGTemplateEngineDemoForIOS
+//
+//  Created by 颜风 on 14-7-12.
+//  Copyright (c) 2014年 Shadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YFAppDelegate class]));
+    }
+}
